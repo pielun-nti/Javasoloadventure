14a58f3c494bb738ab0c3d2a224de4499548bd32
Pierre Lundström <pierre.lundstrom@learnet.se>
2021-02-01 11:04:41 +0100

    changed envexample

--- src/config/Envexample.java
+++ src/config/Envexample.java
@@ -4,7 +4,7 @@ package config;
  * Mvh Pierre
  */
 public class Envexample {
-    public static String dbName = "logs";
+    public static String dbName = "soloadventure";
     public static String driverName ="com.mysql.jdbc.Driver";
     public static String conURL = "jdbc:mysql://localhost:3306/" + dbName + "?characterEncoding=latin1";
     public static String user = "";
